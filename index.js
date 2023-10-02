b1c870c3b73329523bf414c79a571159afa3adf1
Daudi Mwanzia <daudimwanzia@gmail.com>
2023-10-03 00:50:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+// Remove the main#main element
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new h1 element to 'victory'
+newHeader.id = 'victory';
+
+newHeader.textContent = 'DAUDI-MWANZIA is the champion';
+// Append new h1 element to the body
+document.body.appendChild(newHeader);
